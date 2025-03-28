bd64751226951602c013162f16167d45e8d78d9d
alina <alina.meller.98@mail.ru>
2025-03-28 21:17:49 +0300

    28.03.2025

---
+++ 28_03_2025.py
@@ -0,0 +1,37 @@
+# n = int(input())
+#
+# while n != 0:
+#     print(n)
+#     n = int(input())
+
+# while True:
+#     n = int(input())
+#     if n == 0:
+#         break
+#     print(n)
+
+
+# User enters lines. Print each line. If line equals "exit" yoy have to finish loop
+# Use while True loop
+secret = 35
+while True:
+    n = int(input())
+    if n == secret:
+        continue
+    print(n)
+    print(n)
+    print(n)
+    print(n)
+    print(n)
+    print(n)
+    print(n)
+
+
+
+
+
+
+
+
+
+
