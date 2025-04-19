cdd285439a53d0e57cb4193fb3858890db937ef1
alina <alina.meller.98@mail.ru>
2025-04-19 21:53:02 +0300

    19-04-2025

---
+++ 18-04-2025.py
@@ -0,0 +1,65 @@
+line = "hello my friends"
+# print(line[3:8])
+# print(line[:])
+# print(line)
+# print(line[10])
+
+
+# n = input()
+# last = len(n) - 1
+# print(n[last:0:-1])
+
+# name = "Vanya"
+# print(name[::-1])
+
+
+
+# User enters a line. Print this line in reverse order
+# y = input("write something here ")
+# print(y[::-1])
+#
+# User enters a line, start index, end index and step.
+# Print all the letters from start to end with step
+# Vanya is my student
+# 2
+# 6
+# 2
+# nai
+# y = input("write something here ")
+# start = int(input())
+# end = int(input())
+# step = int(input())
+# print(y[start:end + 1:step])
+
+
+# guests = 'Vanya Anton Egor Elya Anna Eva'
+# guest = input('Enter your name: ')
+# if guest in guests:
+#     print('Welcome')
+# else:
+#     print("you shall not pass")
+#
+# if guest not in guests:
+#     print("you shall not pass")
+# else:
+#     print('Welcome')
+
+#
+# User enters shopping list and product.
+
+
+
+
+
+
+
+
+
+
+
+
+list = "brocolli toothpaste twix cucumber"
+confirmation = input("Do you remember the products we need? ")
+if confirmation in list:
+    print("correct✅")
+else: print("absolutely not 😡😡😡")
---
+++ total.py
@@ -0,0 +1,93 @@
+# line1 = "Hello I'm Alina"
+# line2 = '"Привет", - сказала Алина'
+# line3 = '''Hello
+# I'm Alina'''
+# line = 'Vanya'
+# print(line[2])
+# print('Alina'[2])
+# name = input()
+# print(name[1])
+
+
+# Create line 'friends'. Print second letter from this line
+
+
+# LINE55 = input()
+#
+# print(LINE55[int(input())])
+
+# User enTERS
+
+# line = input()
+# print(line[-1])
+
+# speech = "HellomynamneisIvanadilikeicecreamandpizzaandstuffsoyeah"
+# print(speech[-3])
+
+# line = "microphone"
+# print(len(line))
+# print(len('Vanya'))
+# # print(len(5)) Ошибка
+
+
+
+line = "microphonesareusedforsinging"
+length = len(line)
+print(line[length - 1])
+# print("In the word", line, "there are", len(line), "symbols")
+# print("In the word", line, "there are", len(line), "symbols")
+# print(f"In the word {line} there are {len(line)} symbols")
+
+
+
+
+
+
+# User enters a line. Print the last symbol of this word using len()
+
+# variable = "sobasicallyifoundoutthatitaliansreallyhatepineappleonpizzalikechillitisnotabigdealwhysooffended"
+# last_index = len(variable) - 1
+# print(variable[last_index])
+# variable = "sobasicallyifoundoutthatitaliansreallyhatepineappleonpizzalikechillitisnotabigdealwhysooffended"
+# print(variable[0:11])
+
+# x = input()
+# print(x[1:5])
+
+# n = int(input())
+#
+# while n != 0:
+#     print(n)
+#     n = int(input())
+
+# while True:
+#     n = int(input())
+#     if n == 0:
+#         break
+#     print(n)
+
+
+# User enters lines. Print each line. If line equals "exit" yoy have to finish loop
+# Use while True loop
+secret = 35
+while True:
+    n = int(input())
+    if n == secret:
+        continue
+    print(n)
+    print(n)
+    print(n)
+    print(n)
+    print(n)
+    print(n)
+    print(n)
+
+
+
+
+
+
+
+
+
+
