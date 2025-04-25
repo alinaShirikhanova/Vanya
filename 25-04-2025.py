7cfa37a6561c92035116bc958553462209ffd114
alina <alina.meller.98@mail.ru>
2025-04-25 19:45:03 +0300

    25-04-2025

---
+++ 25-04-2025.py
@@ -0,0 +1,49 @@
+line = 'Hello ' *  10
+# print(line)
+from unittest.util import three_way_cmp
+
+# User enters a line and a number (times) Print a line of times entered line
+# hello
+# hellohellohello
+# yyy = input("so i was GOING to go on a self-pitying rant but then i see you so i will save it for later ")
+# times = int(input())
+#
+# print(yyy * times)
+
+# three
+# 5
+# tree
+# first = 'Hello'
+# second = ' my friends'
+# third = first + second
+# print(third)
+
+# line = 'initialization'
+# print(line.count('i'))
+
+
+# line = input()
+# print(line.count("i"))
+
+
+mark1 = 5
+mark2 = 4
+mark3 = 5
+mark4 = 5
+mark5 = 5
+mark6 = 5
+print(mark1, mark2, mark3)
+# list
+
+marks = [5, 4, 5, 5, 4, 4, 4, 5, 4, 4]
+
+print(marks[4])
+
+
+
+
+mark1 = 3
+mark2 = 2
+
+print(mark1, mark2)
+marks_total = [3, 2]
