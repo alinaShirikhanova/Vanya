50e65ad27e2710266f226ea2b720dcdb2608727f
alina <alina.meller.98@mail.ru>
2025-05-03 11:40:58 +0300

    02-05-2025

---
+++ 02-05-2025.py
@@ -0,0 +1,30 @@
+mark1 = 5
+mark2 = 4
+mark3 = 5
+mark4 = 5
+mark5 = 5
+mark6 = 5
+print(mark1, mark2, mark3)
+# list
+
+marks = [5, 'Vanya', True, 5, 4, 4, 4, 5, 4, 4]
+
+print(marks[4])
+
+
+
+
+mark1 = 3
+mark2 = 2
+
+print(mark1, mark2)
+marks_total = [3, 2]
+
+fruits = ['Mango', 'tangerine', 'orange']
+
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+
+for i in range(3):
+    print(fruits[i])
--- 25-04-2025.py
+++ 25-04-2025.py
@@ -26,24 +26,13 @@ from unittest.util import three_way_cmp
 # print(line.count("i"))
 
 
-mark1 = 5
-mark2 = 4
-mark3 = 5
-mark4 = 5
-mark5 = 5
-mark6 = 5
-print(mark1, mark2, mark3)
-# list
-
-marks = [5, 4, 5, 5, 4, 4, 4, 5, 4, 4]
-
-print(marks[4])
 
 
 
 
-mark1 = 3
-mark2 = 2
 
-print(mark1, mark2)
-marks_total = [3, 2]
+
+# Create a list. Print all the elements from this list using for loop
+
+
+
