6c1855205143d21194882b17d0edd3414441dde9
alina <alina.meller.98@mail.ru>
2025-05-17 13:39:21 +0300

    16-05-2025

---
+++ 16-05-2025.py
@@ -0,0 +1,23 @@
+lst = [1, 4, 2, 45, 123, 90]
+
+# Find and print largest number from lst
+
+maxi = 0
+
+for i in range(6):
+    if lst[i] > maxi:
+        maxi = lst[i]
+
+
+# if lst[0] > maxi:
+#     maxi = lst[0]
+# if lst[1] > maxi:
+#     maxi = lst[1]
+# if lst[2] > maxi:
+#     maxi = lst[2]
+# if lst[0] > maxi:
+#     maxi = lst[0]
+# if lst[1] > maxi:
+#     maxi = lst[1]
+# if lst[2] > maxi:
+#     maxi = lst[2]
