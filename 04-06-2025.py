55f51d72eb768741f308d04a25e477511e2fefae
alina <alina.meller.98@mail.ru>
2025-06-05 11:04:51 +0300

    05-06-2025

---
+++ 04-06-2025.py
@@ -0,0 +1,48 @@
+cities = [
+    "Amsterdam", "Moscow", "Warsaw", "Vienna", "Athens",
+    "Stockholm", "Madrid", "Lisbon", "Berlin", "Nice",
+    "Edinburgh", "Hamburg", "Geneva", "Oslo", "London",
+    "Paris", "Rome", "Prague", "Budapest", "Brussels",
+    "Copenhagen", "Dublin", "Florence", "Helsinki", "Istanbul",
+    "Jakarta", "Kyiv", "Lviv", "Munich", "Naples",
+    "Odessa", "Porto", "Quito", "Riga", "Seville",
+    "Tallinn", "Utrecht", "Valencia", "Xiamen",
+    "Yerevan", "Zurich", "Bucharest", "Krakow", "Ljubljana",
+    "Bratislava", "Zagreb", "Tirana", "Sarajevo", "Chisinau",
+    "Skopje", "Podgorica", "Luxembourg", "Vilnius", "Sofia",
+    "Antwerp", "Bordeaux", "Cologne", "Dresden", "Eindhoven",
+    "Frankfurt", "Graz", "Hanover", "Innsbruck", "Jena",
+    "Kassel", "Linz", "Mainz", "Nuremberg", "Oldenburg",
+    "Potsdam", "Regensburg", "Stuttgart", "Toulouse", "Ulm",
+    "Versailles", "Wuppertal", "Xanten", "York", "Zwickau",
+    "Agra", "Bangalore", "Chennai", "Delhi", "Ernakulam",
+    "Fukuoka", "Genoa", "Hiroshima", "Izmir", "Jeonju",
+    "Kobe", "Lahore", "Manila", "Nagoya", "Okayama",
+    "Pune", "Quezon", "Rajkot", "Sendai", "Tokyo",
+    "Ulsan", "Varanasi", "Wuhan", "Xalapa", "Yokohama"
+]
+
+# user_city = 'Manila'
+# # print(user_city in cities)
+#
+# if user_city in cities:
+#     print('Круто. Корректный город')
+# else:
+#     print('Такого города нет')
+#
+# used_cities = []
+# user_choice = input()
+
+# used_cities.append(user_choice)
+# used_cities.append('Vanya1')
+# used_cities.append('Vanya2')
+# used_cities.append('Vanya3')
+
+user_choice = 'London'
+last_letter = user_choice[-1]
+
+
+
+
+
+
